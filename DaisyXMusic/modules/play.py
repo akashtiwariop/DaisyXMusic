da99b3771b4692ff246f104bf904f42dd80fd386
akashtiwariop <84773442+akashtiwariop@users.noreply.github.com>
2021-08-18 16:11:08 +0530

    Update play.py

--- DaisyXMusic/modules/play.py
+++ DaisyXMusic/modules/play.py
@@ -637,7 +637,7 @@ async def play(_, message: Message):
             toxxt = "**Select the song you want to play**\n\n"
             j = 0
             useer=user_name
-            emojilist = ["1️⃣","2️⃣","3️⃣","4️⃣","5️⃣",]
+            emojilist = ["⓵","⓶","⓷","⓸","⓹",]
 
             while j < 5:
                 toxxt += f"{emojilist[j]} **Title - [{results[j]['title']}](https://youtube.com{results[j]['url_suffix']})**\n"
@@ -649,13 +649,13 @@ async def play(_, message: Message):
             koyboard = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("1️⃣", callback_data=f'plll 0|{query}|{user_id}'),
-                        InlineKeyboardButton("2️⃣", callback_data=f'plll 1|{query}|{user_id}'),
-                        InlineKeyboardButton("3️⃣", callback_data=f'plll 2|{query}|{user_id}'),
+                        InlineKeyboardButton("⓵", callback_data=f'plll 0|{query}|{user_id}'),
+                        InlineKeyboardButton("⓶", callback_data=f'plll 1|{query}|{user_id}'),
+                        InlineKeyboardButton("⓷", callback_data=f'plll 2|{query}|{user_id}'),
                     ],
                     [
-                        InlineKeyboardButton("4️⃣", callback_data=f'plll 3|{query}|{user_id}'),
-                        InlineKeyboardButton("5️⃣", callback_data=f'plll 4|{query}|{user_id}'),
+                        InlineKeyboardButton("⓸", callback_data=f'plll 3|{query}|{user_id}'),
+                        InlineKeyboardButton("⓹", callback_data=f'plll 4|{query}|{user_id}'),
                     ],
                     [
                         InlineKeyboardButton(text="Close 🛑", callback_data="cls")
